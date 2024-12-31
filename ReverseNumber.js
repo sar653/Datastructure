b3d1131ee38d4438ada90cfc5832085622aa51f2
sar653 <sarva653@gmail.com>
2024-12-31 10:55:59 +0530

    reversednumber

--- ReverseNumber.js
+++ ReverseNumber.js
@@ -1,8 +1,19 @@
-function ReverseNumber( num) {
+function ReverseNumber(num) {
+    let isNegative = num < 0;
+  num = Math.abs(num);
+  
+  let result = 0;
+//   let result="" if need to include 0
+  while (num > 0) {
+    let lastnumber = num % 10;
+    result = result * 10 + lastnumber;
+    //result=result+lastnumber
 
+    num = Math.floor(num / 10);
+  }
 
-    
-    
+  return isNegative? -result:result
 }
 
-const reversed=ReverseNumber(12345)
+const reversed = ReverseNumber(-12345);
+console.log(reversed);
